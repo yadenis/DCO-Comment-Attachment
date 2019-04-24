7088c2a42acb4c40e4f6a257499546402f65946d
Denis Yanchevskiy <denis@denisco.pro>
2019-04-24 05:03:45 +0300

    Initialize

---
+++ dco-comment-attachment.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * DCO Comment Attachment
+ *
+ * @package DCO_Comment_Attachment
+ * @author Denis Yanchevskiy
+ * @copyright 2019
+ * @license GPLv2+
+ *
+ * @since 1.0
+ *
+ * Plugin Name: DCO Comment Attachment
+ * Description: Allows your visitors to attach files with their comments
+ * Version: 1.0
+ * Author: Denis Yanchevskiy
+ * Author URI: http://denisco.pro
+ * License: GPLv2 or later
+ * Text Domain: dco-comment-attachment
+ */
+
+defined( 'ABSPATH' ) || die;
+
+define( 'DCO_CA_URL', plugin_dir_url( __FILE__ ) );
+define( 'DCO_CA_PATH', plugin_dir_path( __FILE__ ) );
+define( 'DCO_CA_BASENAME', plugin_basename( __FILE__ ) );
+define( 'DCO_CA_VERSION', '1.0' );
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== DCO Comment Attachment ===
+Contributors: denisco
+Tags: comment attachment
+Requires at least: 4.6
+Tested up to: 5.1
+Requires PHP: 5.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows your visitors to attach files with their comments
+
+== Description ==
+DCO Comment Attachment allows your visitors to attach files with their comments, such as documents and images.
+
+== Installation ==
+1. Upload `dco-comment-attachment` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release
